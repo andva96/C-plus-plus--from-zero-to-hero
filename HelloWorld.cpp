e575914f98cfac5678958bca771f7159a25fadc6
andva96 <andvaesc@gmail.com>
2024-05-19 10:59:59 -0500

    Initial commit - hello world file.

---
+++ HelloWorld.cpp
@@ -0,0 +1,8 @@
+// HelloWorld Programm in C++
+#include <iostream> //Pre-processor directive used to include files in the program. - iostream file.
+// needed for cout.
+
+int main() {
+    std::cout << "Hello World!";
+    return 0; //Exit status of the program. 
+}
