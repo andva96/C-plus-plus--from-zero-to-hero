37810856b3410f5f1b7294c028a559696290ec99
andva96 <andvaesc@gmail.com>
2024-05-21 08:12:28 -0500

    Modify hello world and add variables and data types short explanation.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,11 @@
-// HelloWorld Programm in C++
-#include <iostream> //Pre-processor directive used to include files in the program. - iostream file.
-// needed for cout.
+// HELLO WORLD PROGRAM IN C++
+
+/* HEADER FILE LIBRARY: Pre-processor directive used to include files in the program.
+   It allows to work with input and output objects, such as cout.
+   Everything that comes with a # is a pre-processing statement.*/
+#include <iostream> 
 
 int main() {
-    std::cout << "Hello World!";
-    return 0; //Exit status of the program. 
+    std::cout << "Hello World! \n"; // cout: object usted together with << to print text.
+    return 0; //Exit status of the program. Not necessary in main function (it assumes 0 when you don't write anything). 
 }
